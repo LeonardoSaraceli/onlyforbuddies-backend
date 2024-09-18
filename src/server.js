ad92a22a7e4016f6d4625e7501261cf10a095614
LeonardoSaraceli <leonardolodi09@gmail.com>
2024-09-18 21:46:30 +0100

    Feat: set up express and prisma

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import 'dotenv/config'
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+
+export const app = express()
+
+app.use(morgan('dev'))
+app.use(cors())
+app.use(express.json())
