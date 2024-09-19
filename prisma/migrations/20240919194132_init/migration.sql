8463adb3fb711c6f8b633dd24d75d20c34edef99
LeonardoSaraceli <leonardolodi09@gmail.com>
2024-09-19 20:45:31 +0100

    Feat: create entities and their relantionships

---
+++ prisma/migrations/20240919194132_init/migration.sql
@@ -0,0 +1,185 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'USER');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Variant" (
+    "id" SERIAL NOT NULL,
+    "sku" TEXT NOT NULL,
+    "color" TEXT NOT NULL,
+    "hex" CHAR(7) NOT NULL,
+    "price" DECIMAL(10,2) NOT NULL,
+    "product_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Variant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Image" (
+    "id" SERIAL NOT NULL,
+    "url" TEXT NOT NULL,
+    "variant_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Image_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" SERIAL NOT NULL,
+    "total" DECIMAL(10,2) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "user_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProductOrder" (
+    "id" SERIAL NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "variant_id" INTEGER NOT NULL,
+    "order_id" INTEGER NOT NULL,
+
+    CONSTRAINT "ProductOrder_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Address" (
+    "id" SERIAL NOT NULL,
+    "street" TEXT NOT NULL,
+    "number" INTEGER NOT NULL,
+    "complement" TEXT,
+    "region" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "country" TEXT NOT NULL,
+    "zip" CHAR(9) NOT NULL,
+    "main" BOOLEAN NOT NULL DEFAULT true,
+    "user_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Address_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Cart" (
+    "id" SERIAL NOT NULL,
+    "total" DECIMAL(10,2) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "user_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Cart_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProductCart" (
+    "id" SERIAL NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "cart_id" INTEGER NOT NULL,
+    "variant_id" INTEGER NOT NULL,
+
+    CONSTRAINT "ProductCart_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Size" (
+    "id" SERIAL NOT NULL,
+    "size" TEXT NOT NULL,
+
+    CONSTRAINT "Size_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProductSize" (
+    "id" SERIAL NOT NULL,
+    "variant_id" INTEGER NOT NULL,
+    "size_id" INTEGER NOT NULL,
+
+    CONSTRAINT "ProductSize_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_CategoryToProduct" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Variant_sku_key" ON "Variant"("sku");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Cart_user_id_key" ON "Cart"("user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_CategoryToProduct_AB_unique" ON "_CategoryToProduct"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_CategoryToProduct_B_index" ON "_CategoryToProduct"("B");
+
+-- AddForeignKey
+ALTER TABLE "Variant" ADD CONSTRAINT "Variant_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Image" ADD CONSTRAINT "Image_variant_id_fkey" FOREIGN KEY ("variant_id") REFERENCES "Variant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductOrder" ADD CONSTRAINT "ProductOrder_variant_id_fkey" FOREIGN KEY ("variant_id") REFERENCES "Variant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductOrder" ADD CONSTRAINT "ProductOrder_order_id_fkey" FOREIGN KEY ("order_id") REFERENCES "Order"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Address" ADD CONSTRAINT "Address_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Cart" ADD CONSTRAINT "Cart_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductCart" ADD CONSTRAINT "ProductCart_cart_id_fkey" FOREIGN KEY ("cart_id") REFERENCES "Cart"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductCart" ADD CONSTRAINT "ProductCart_variant_id_fkey" FOREIGN KEY ("variant_id") REFERENCES "Variant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductSize" ADD CONSTRAINT "ProductSize_variant_id_fkey" FOREIGN KEY ("variant_id") REFERENCES "Variant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductSize" ADD CONSTRAINT "ProductSize_size_id_fkey" FOREIGN KEY ("size_id") REFERENCES "Size"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CategoryToProduct" ADD CONSTRAINT "_CategoryToProduct_A_fkey" FOREIGN KEY ("A") REFERENCES "Category"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CategoryToProduct" ADD CONSTRAINT "_CategoryToProduct_B_fkey" FOREIGN KEY ("B") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ prisma/seed.js
@@ -0,0 +1,91 @@
+import { prisma } from '../src/utils/prisma.js'
+import bcrypt from 'bcrypt'
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      email: 'leonardolodi09@gmail.com',
+      password: await bcrypt.hash('123', 10),
+      role: 'ADMIN',
+    },
+  })
+
+  const product = await prisma.product.create({
+    data: {
+      name: 'The Buddies Strikes Again',
+      description: 'Camisa de malha',
+    },
+  })
+
+  const variant = await prisma.variant.create({
+    data: {
+      sku: '246/789/20',
+      color: 'Off-white',
+      hex: '#FAF9F6',
+      price: 180.99,
+      product_id: product.id,
+    },
+  })
+
+  await prisma.image.create({
+    data: {
+      url: 'https://www.ideiapack-online.pt/WebRoot/ce_pt/Shops/960329864/6464/E8ED/C60D/930A/BD65/C0A8/1911/FF81/991512.4XL.jpg',
+      variant_id: variant.id,
+    },
+  })
+
+  await prisma.address.create({
+    data: {
+      street: 'Rua Dr. Joaquim Pires de Lima',
+      number: 100,
+      complement: '2° andar direito',
+      region: 'Paranhos',
+      city: 'Porto',
+      state: 'Porto',
+      country: 'Portugal',
+      zip: '12345-678',
+      user_id: user.id,
+    },
+  })
+
+  await prisma.size.createMany({
+    data: [
+      {
+        size: 'P',
+      },
+      {
+        size: 'M',
+      },
+      {
+        size: 'G',
+      },
+    ],
+  })
+
+  await prisma.productSize.createMany({
+    data: [
+      {
+        size_id: 1,
+        variant_id: 1,
+      },
+      {
+        size_id: 2,
+        variant_id: 1,
+      },
+      {
+        size_id: 3,
+        variant_id: 1,
+      },
+    ],
+  })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
